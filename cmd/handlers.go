67fa24092d7a05124a9b957e113b22ffbd95a3a9
qooked <qooked@gmail.com>
2024-02-06 12:33:43 +0300

    jopaScriptUpdated

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -25,8 +25,22 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	ts, _ := template.ParseFiles("./ui/html/registerpage.html")
-	ts.Execute(w, "")
+	switch r.Method {
+	case "GET":
+		ts, _ := template.ParseFiles("./ui/html/registerpage.html")
+		ts.Execute(w, "")
+		break
+	case "POST":
+		err := r.ParseMultipartForm(0)
+		if err != nil {
+			log.Printf("error")
+		}
+		name := r.FormValue("name")
+		surname := r.FormValue("surname")
+		email := r.FormValue("email")
+		password := r.FormValue("password1")
+		log.Println(name, surname, email, password)
+	}
 }
 
 func mypage(w http.ResponseWriter, r *http.Request) {
--- ui/static/js/registerpage.js
+++ ui/static/js/registerpage.js
@@ -1,23 +1,13 @@
 function validatePassword() {
     const password1 = document.getElementById('password1').value;
     const password2 = document.getElementById('password2').value;
+    const email = document.getElementById('email').value;
     const regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
     const pattern  = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    const name1 = document.getElementById('name').value;
-    const surname = document.getElementById('surname').value;
-    const email = document.getElementById('email').value;
     const minNumberofChars = 8;
     const maxNumberofChars = 16;
     if(!pattern.test(email)) {
         alert("Email введен не верно");
-        return false;
-    }
-    if (name1 === '' || surname === '' || password1 === '' || password2 === '' || email === ''){
-        alert("Заполните пустые поля");
-        return false;
-    }
-    if(password1 != password2) {
-        alert("Пароли не совпадают");
         return false;
     }
     if(password1.length < minNumberofChars || password1.length > maxNumberofChars){
@@ -28,30 +18,30 @@ function validatePassword() {
         alert("Пароль должен включать хотя бы одну цифру и специальный символ.");
         return false;
     }
+    if(password1 != password2) {
+        alert("Пароли не совпадают");
+        return false;
+    }
     return true;
 }
-function submitForm() {
-    var form = document.getElementById("registrationForm");
-    var formData = new FormData(form);
 
-    fetch("/register", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Тут можешь обработать ответ от сервера
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
-}
+const form = document.getElementById("registrationForm");
 
-function checker(){
-    if (validatePassword() == false){
-    }else{
-        window.location.href = "/login";
+form.onsubmit = async (e) => {
+    e.preventDefault();
+    if (!validatePassword()){
+        return
     }
-}
-
-button.addEventListener('click', checker);
+    const fd = new FormData(form);
+        console.log(form);
+        await fetch("/register", {
+        method: "POST",
+        body: fd
+        }).then(async (response) => {
+        console.log(response.status);
+        if (response.status === 200) {
+            window.location.href = "/login";
+            return;
+        }
+    });
+};
