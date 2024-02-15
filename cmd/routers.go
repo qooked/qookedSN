48f0dbd485e843c80eef29d3ff364463b3c5804f
Леонид Мизгирев <qooked@gmail.com>
2024-02-15 12:31:06 +0300

    test

--- cmd/routers.go
+++ cmd/routers.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func router() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/register", register)
-	mux.HandleFunc("/compare-tokens", compareTokens)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/change-friend-status", changeFriendStatus)
-	mux.HandleFunc("/check-friend-status", checkFriendStatus)
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
+func router() *mux.Router {
+	gmux := mux.NewRouter()
+	gmux.HandleFunc("/", indexHandler)
+	gmux.HandleFunc("/login", login)
+	gmux.HandleFunc("/register", register)
+	gmux.HandleFunc("/compare-tokens", compareTokens)
+	gmux.HandleFunc("/logout", logout)
+	gmux.HandleFunc("/change-friend-status", changeFriendStatus)
+	gmux.HandleFunc("/check-friend-status", checkFriendStatus)
+	staticDir := http.Dir("./ui/static")
+	staticHandler := http.StripPrefix("/static/", http.FileServer(staticDir))
+	gmux.PathPrefix("/static/").Handler(staticHandler)
+	return gmux
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require golang.org/x/crypto v0.18.0
 require github.com/go-sql-driver/mysql v1.7.1
 
 require github.com/golang-jwt/jwt v3.2.2+incompatible
+
+require github.com/gorilla/mux v1.8.1 // indirect
